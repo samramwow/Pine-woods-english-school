88e9c05a0a3faeba58a6b987d3eebd7936cddb08
samramwow <nrao88940@gmail.com>
2026-04-11 05:47:50 +0530

    style.cs

---
+++ style.cs
@@ -0,0 +1,15 @@
+.navbar {
+  background-color: #333; /* Dark background */
+  padding: 1rem;
+}
+
+.nav-links {
+  display: flex;
+  list-style-type: none; /* Remove bullets */
+  margin: 0;
+  padding: 0;
+}
+
+.nav-links li {
+  margin-right: 20px; /* Space between links */
+}
